d3fa3f887b59ed1ea90f17186b995d8ef276655f
tfquirk <timothyfquirk@gmail.com>
2019-04-30 15:29:10 -0400

    Player model created

---
+++ db/migrate/20190430192849_create_players.rb
@@ -0,0 +1,11 @@
+class CreatePlayers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :players do |t|
+      t.string :email
+      t.string :username
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
