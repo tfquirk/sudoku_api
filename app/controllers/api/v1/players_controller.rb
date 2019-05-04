331ccbc7a5b1e07745f4709143f414aabf29d329
tfquirk <timothyfquirk@gmail.com>
2019-05-04 12:29:44 -0400

    Players controller methods added

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::PlayersController < ApplicationController
+
+  def index
+    @players = Player.all
+    render json: @players, status: :ok
+  end
+
+  def show
+    @player = Player.find(params[:id])
+    render json: @player, status: :ok
+  end
+
 end
