07adf4442708fce4b6fd4969dfeb5f61b7827824
tfquirk <timothyfquirk@gmail.com>
2019-05-04 12:29:04 -0400

    Games controller methods added

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::GamesController < ApplicationController
+
+  def index
+    @games = Game.all
+    render json: @games, status: :ok
+  end
+
+  def show
+    @game = Game.find(params[:id])
+    render json: @game, status: :ok
+  end
+
 end
