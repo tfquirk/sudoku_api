cb65855437a7954f73d88c61fa72aa93e0932e91
tfquirk <timothyfquirk@gmail.com>
2019-05-04 12:28:17 -0400

    Boards controller methods added

--- app/controllers/api/v1/boards_controller.rb
+++ app/controllers/api/v1/boards_controller.rb
@@ -2,7 +2,11 @@ class Api::V1::BoardsController < ApplicationController
 
   def index
     @boards = Board.all
-
     render json: @boards, status: :ok
+  end
+
+  def show
+    @board = Board.find(params[:id])
+    render json: @board, status: :ok
   end
 end
