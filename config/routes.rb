cc3d9f9a95943dae55879894cce945385198ca3a
tfquirk <timothyfquirk@gmail.com>
2019-05-04 11:20:12 -0400

    added initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  namespace :api do
+   namespace :v1 do
+     resources :boards, only: [:index, :show]
+     resources :games, only: [:index, :show]
+     resources :players, only: [:index, :show]
+
+     #get "auto_login", to: "auth#auto_login"
+     #get "team_players", to: "players#team_players"
+     #post "/signup", to: "auth#signup_user"
+     #post "/login", to: "auth#login_user"
+
+    end
+  end
+
+
+
 end
